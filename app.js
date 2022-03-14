af14424922e6e64a7fcda5d99ee6f9bc959740f0
ambernluu <85398285+ambernluu@users.noreply.github.com>
2022-03-14 16:52:42 -0400

    add colors

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ document.addEventListener('mousemove', function(e){
    const r =  Math.round(e.pageX * 255 / window.innerWidth);
    const b = Math.round(e.pageY * 255 / window.innerHeight);
 //    console.log(`New coords: ${r}, ${b}`);
-   document.body.style.backgroundColor = `rgb(${r}, 0, ${b})`;
+   console.log(document.body.style.backgroundColor = `rgb(${r}, 0, ${b})`);
 });
 
